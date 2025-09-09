28e28f1df7a2d914190016b1a8af6996c0d71c87
leonardo <leonardojermano43@gmail.com>
2025-09-08 22:04:12 -0300

    Responsividade e MenuNavBar

--- src/js/mobile-navbar.js
+++ src/js/mobile-navbar.js
@@ -1,46 +1,41 @@
 class MobileNavbar {
-    constructor(mobileMenuSelector, menuSelector, menuLinksSelector) {
-        this.mobileMenu = document.querySelector(mobileMenuSelector);
-        this.menu = document.querySelector(menuSelector);
-        this.menuLinks = document.querySelectorAll(menuLinksSelector);
-        this.activeClass = "active";
+  constructor(mobileMenuSelector, menuSelector, menuLinksSelector) {
+    this.mobileMenu = document.querySelector(mobileMenuSelector);
+    this.menu = document.querySelector(menuSelector);
+    this.menuLinks = document.querySelectorAll(menuLinksSelector);
+    this.activeClass = "active";
 
-        this.handleClick = this.handleClick.bind(this);
-    }
+    this.handleClick = this.handleClick.bind(this);
+  }
 
-    animateLinks(){
-        this.menuLinks.forEach((link, index) => {
-            link.style.animation
-                ? (link.style.animation = "")
-                : link.style.animation = `menuLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-
-
+  animateLinks() {
+    this.menuLinks.forEach((link, index) => {
+      link.style.animation
+        ? (link.style.animation = "")
+        : (link.style.animation = `menuLinkFade 0.5s ease forwards ${
+            index / 7 + 0.3
+          }s`);
     });
-    }
+  }
 
-    handleClick(){
-        this.menu.classList.toggle(this.activeClass);
-        this.mobileMenu.classList.toggle(this.activeClass);
-        this.animateLinks();
-    }
-     
+  handleClick() {
+    this.menu.classList.toggle(this.activeClass);
+    this.mobileMenu.classList.toggle(this.activeClass);
+    this.animateLinks();
+  }
 
-    addClickEvent() {
-        this.mobileMenu.addEventListener("click", this.handleClick);
-    }   
+  addClickEvent() {
+    this.mobileMenu.addEventListener("click", this.handleClick);
+  }
 
-    init() {
-        if (this.mobileMenu){
-            this.addClickEvent();
-        }
-        return this;
+  init() {
+    if (this.mobileMenu) {
+      this.addClickEvent();
     }
+    return this;
+  }
 }
 
-const mobileNavbar = new MobileNavbar(
-    ".mobileMenu",
-    ".menu",
-    ".menuLinks li"
-);
+const mobileNavbar = new MobileNavbar(".mobileMenu", ".menu", ".menuLinks li");
 
 mobileNavbar.init();
--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,11 @@
-        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-        anchor.addEventListener('click', function (e) {
-            const target = document.querySelector(this.getAttribute('href'));
-            if (target) {
-        e.preventDefault();
-        target.scrollIntoView({ behavior: 'smooth' });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      e.preventDefault();
+      target.scrollIntoView({ behavior: "smooth" });
     }
-            });
-        });
+  });
+});
 
-        AOS.init();
-
+AOS.init();
